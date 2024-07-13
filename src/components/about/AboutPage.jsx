3f0d2318b31fad17101ad4b1182ef57918dbb966
dayysss <ferdayganz@gmail.com>
2024-07-13 23:24:52 +0700

    tidur

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, React } from "react";
 import { motion } from "framer-motion";
 import Slider from "react-slick";
 import { styles } from "../../styles";
